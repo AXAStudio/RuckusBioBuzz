49ec0cbf2490f138f79e29c90766e1fcdcbd2c06
Andres Alonso <108694498+AXAStudio@users.noreply.github.com>
2026-05-16 20:03:16 -0400

    custom visualizer

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/ExampleSwerveAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/ExampleSwerveAuto.java
@@ -11,8 +11,8 @@ import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 
 @Autonomous(name = "ExampleSwerveAuto", group = "Auto")
 public class ExampleSwerveAuto extends OpMode {
-    private static final Pose START_POSE = new Pose(72.0, 72.0, 0.0);
-    private static final Pose END_POSE = new Pose(96.0, 72.0, 0.0);
+    private static final PathStep START_STEP = new PathStep(72.0, 72.0, 0.0);
+    private static final PathStep END_STEP = new PathStep(96.0, 72.0, 0.0);
 
     private Follower follower;
     private Path driveForward;
@@ -22,7 +22,7 @@ public class ExampleSwerveAuto extends OpMode {
     @Override
     public void init() {
         follower = Constants.createFollower(hardwareMap);
-        follower.setStartingPose(START_POSE);
+        follower.setStartingPose(START_STEP.toPose());
 
         buildPaths();
         updateTelemetry("Initialized");
@@ -39,7 +39,7 @@ public class ExampleSwerveAuto extends OpMode {
         pathStarted = true;
         pathFinished = false;
 
-        follower.setStartingPose(START_POSE);
+        follower.setStartingPose(START_STEP.toPose());
         follower.followPath(driveForward, true);
     }
 
@@ -66,8 +66,11 @@ public class ExampleSwerveAuto extends OpMode {
     }
 
     private void buildPaths() {
-        driveForward = new Path(new BezierLine(START_POSE, END_POSE));
-        driveForward.setConstantHeadingInterpolation(START_POSE.getHeading());
+        Pose startPose = START_STEP.toPose();
+        Pose endPose = END_STEP.toPose();
+
+        driveForward = new Path(new BezierLine(startPose, endPose));
+        driveForward.setConstantHeadingInterpolation(startPose.getHeading());
     }
 
     private void updateTelemetry(String state) {
